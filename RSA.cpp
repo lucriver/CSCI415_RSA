efd238993bdc325ce159c6f81744b5b1f0c73a26
lucas <lriv@DESKTOP-EN3V0A9>
2023-03-08 15:26:09 -0600

    TODO: encryption and decryption for RSA.

--- RSA.cpp
+++ RSA.cpp
@@ -24,14 +24,19 @@ public:
   BigInt getPrivateKey() const;
   BigInt getKeyModulo() const;
 
-  // encrypt()
-  // decrypt()
+  // TODO- these need to be worked on
+  std::string encrypt(std::string) const;
+  std::string decrypt(std::string) const;
 
   // ---- debugging function, outputs all class member values
   void debug();
 
 private:
-  BigInt p, q, n, phi_n, e, d;
+  BigInt p, q; // primes p and q
+  BigInt n;    // modulo used with keys
+  BigInt phi_n;
+  BigInt e;    // public key
+  BigInt d;    // private key
   BigInt generateRandomPrime(const int) const;
   BigInt randomBigInt(const int) const;
   BigInt randomBigIntInRange(const BigInt, const BigInt) const;
@@ -48,10 +53,11 @@ inline
 void RSA::debug() {
   std::cout << "p: " << p << std::endl;
   std::cout << "q: " << q << std::endl;
-  std::cout << "n: " << n << std::endl;
-  std::cout << "phi(n): " << getKeyModulo() << std::endl;
+  std::cout << "n: " << getKeyModulo() << std::endl;
+  std::cout << "phi(n): " << phi_n << std::endl;
   std::cout << "e (public key): " << getPublicKey() << std::endl;
   std::cout << "d (private key): " << getPrivateKey() << std::endl;
+
 }
 
 
@@ -69,42 +75,33 @@ RSA::RSA(const int decimal_digits_count) {
     throw std::invalid_argument(s);
   }
 
-  // define primes p and q that were verified with the miller-rabin method
+  // calculate random primes p and q of lenght decimal_digits_count
   std::cout << "Initializing system primes..." << std::endl;
   p = generateRandomPrime(decimal_digits_count);
   q = generateRandomPrime(decimal_digits_count);
   std::cout << "System primes initialized." << std::endl;
 
+  n = BigInt((p * q));                                // calculate n
+  phi_n = BigInt((p - BigInt(1)) * (q - BigInt(1)));  // calcualte phi_n
+
   std::cout << "Calculating system keys..." << std::endl;
-  // define n
-  n = BigInt((p * q));
-
-  // define modulo (or alternatively phi(n) in textbook)
-  phi_n = BigInt((p - BigInt(1)) * (q - BigInt(1)));
-
-  // define public key (or alternatively e in textbook)
-  for (int i = 2; BigInt(i) < phi_n; i++) {
-    BigInt res = gcd(BigInt(i), phi_n);
-    if (res == BigInt(1)) {
-      e = BigInt(i);
+  for (BigInt i = 2; i < phi_n; i = i + 1) { // calculate public key e such that gcd(phi_n,e) = 1 for 1 < e < phi_n
+    if (gcd(i, phi_n) == 1) {
+      e = i;
       break;
     }
   }
-  if (e <= BigInt(1) || e >= phi_n) {
+  if (e <= BigInt(1) || e >= phi_n) {       // sanity check- this condition should never be true
     throw std::logic_error("Calculated euler totient is of incorrect value. Try again");
   }
-
-  // define private key (or alternatively d in textbook)
-  d = euclidsExtended(e, phi_n);
-
-  // final verification
-  if (((e * d) % phi_n) != BigInt(1)) {
+  d = euclidsExtended(e, phi_n);            // calculate private key d
+  if (((e * d) % phi_n) != BigInt(1)) {     // another sanity check- this condition should never be true
     throw std::logic_error("Variables produced violate requirements for RSA. Try again");
   }
   std::cout << "System keys initialized." << std::endl;
+
   std::cout << "RSA crypto-system initialized." << std::endl;
 }
-
 
 inline
 RSA::~RSA() {}
@@ -127,6 +124,28 @@ BigInt RSA::getKeyModulo() const {
   return n;
 }
 
+// TODO
+inline
+std::string RSA::encrypt(std::string m) const {
+  BigInt plaintext(m);
+  BigInt encoded_plaintext = fastModExpBigInt(plaintext, getPublicKey(), getKeyModulo());
+  std::stringstream s;
+  s << encoded_plaintext;
+  std::string ciphertext = s.str();
+  return ciphertext;
+}
+
+// TODO 
+inline
+std::string RSA::decrypt(std::string c) const {
+  BigInt ciphertext(c);
+  BigInt decoded_ciphertext = fastModExpBigInt(ciphertext, getPrivateKey(), getKeyModulo());
+  std::stringstream s;
+  s << decoded_ciphertext;
+  std::string plaintext = s.str();
+  return plaintext;
+}
+
 // ****************************************
 
 
@@ -140,7 +159,7 @@ BigInt RSA::generateRandomPrime(const int decimal_digits_count) const {
   std::random_device rd;      // generate seed for random number generator (rng)
   std::mt19937_64 rng(rd());  // random number generator
 
-  // usedd to generate a seq. of uniformly distributed random digits between 0-9
+  // used to generate a seq. of uniformly distributed random digits between 0-9
   std::uniform_int_distribution<int> dist(0, 9);
 
   // create a "decimal_digits"-digits random number
@@ -154,24 +173,24 @@ BigInt RSA::generateRandomPrime(const int decimal_digits_count) const {
   // use miller-rabin to determine if rand_num is prime
   std::cout << "Looking for primes..." << std::endl;
   int counter = 0;
-  const int reset_interval = 200; // interval by which shuffling prime candidate should be aborted
-  const int max_evals = 5000;
-  const int rounds = 40;
+  const int reset_interval = 200; // when counter == reset_interval, stop shuffling and make new prime candidate
+  const int max_evals = 5000;     // max number of prime candidates to check
+  const int rounds = 40;          // number of rounds for miller-rabin algorithm
   while (!isPrimeMRT(BigInt(rand_num), rounds)) { // while prime candidate is not prime by miller-rabin method
     counter++;
     std::cout << "Prime candidates evaluated: " << counter;
-    if (counter % reset_interval == 0) {
+    if (counter % reset_interval == 0) {        // if we have checked another 200 prime candidates
       if (counter == max_evals) {               // if we have evaluated too many potential primes, throw exception
         throw std::runtime_error("Timeout on prime number generation. Please try again.");
       }
-      else {
+      else {                                    // create a completely new prime candidate
         for (int i = 1; i < decimal_digits_count - 1; i++) {
           std::string rand_digit = std::to_string(dist(rng)); // get 1-digit rand between 0-9
           rand_num[i] = rand_digit[0];
         }
       }
     }
-    else {
+    else {                                      // if we have not checked another 200 prime candidates
       // shuffle every digit of the prime candidate, except first and last digit
       std::shuffle(rand_num.begin() + 1, rand_num.end() - 1, rng);
     }
@@ -243,10 +262,11 @@ BigInt RSA::randomBigInt(const int digits_count) const {
   std::stringstream ss;
   std::random_device rd;
 
-  while (ss.tellp() < digits_count) {
-    ss << rd();
+  while (ss.tellp() < digits_count) { // while current random number is not enough digits in length
+    ss << rd();                       // add new random number to the current random number
   }
 
+  // convert current random number to string, truncate digits if number has too many digits
   std::string randomDigits = ss.str().substr(0, digits_count);
   return BigInt(randomDigits);
 }
@@ -270,8 +290,8 @@ BigInt RSA::randomBigIntInRange(const BigInt low, const BigInt high) const {
 }
 
 // info: implements the fast modular exponentiation algorithm in project requirements for BigInts
-// params: BigInt's a, b and n
-// returns: (a^b) mod (n)
+// params: BigInt's a, b and m
+// returns: (a^b) mod (m)
 inline
 BigInt RSA::fastModExpBigInt(BigInt a, BigInt b, BigInt m) const {
   BigInt f(1);
@@ -287,9 +307,9 @@ BigInt RSA::fastModExpBigInt(BigInt a, BigInt b, BigInt m) const {
   return f;
 }
 
-// // info: implements the fast modular exponentiation algorithm in project requirements
-// // params: long long's a, b and n
-// // returns: (a^b) mod (n)
+// info: implements the fast modular exponentiation algorithm in project requirements
+// params: long long's a, b and m
+// returns: (a^b) mod (m)
 inline
 long long RSA::fastModExp(long long a, long long int b, long long m) const {
   long long f = 1;
--- driver.cpp
+++ driver.cpp
@@ -3,7 +3,6 @@
 
 #include "RSA.cpp"
 
-
 int main() {
   // specify how many digits our RSA primes should be
   int prime_digits = 101;
@@ -13,6 +12,14 @@ int main() {
 
   // check initialized class values
   rsa_0.debug();
+
+  // example
+  std::string message = "17";
+  std::string encoded_message = rsa_0.encrypt(message);
+  std::string decoded_message = rsa_0.decrypt(encoded_message);
+  std::cout << "plaintext: " << message << std::endl;
+  std::cout << "ciphertext: " << encoded_message << std::endl;
+  std::cout << "decoded ciphertext: " << decoded_message << std::endl;
 
   return 0;
 }
