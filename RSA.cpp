3221a0dddf89f31620c86b42b95c3c86c1845905
lucas <lriv@DESKTOP-EN3V0A9>
2023-03-11 14:08:34 -0600

    Added codebook struct constant for null character.

--- RSA.cpp
+++ RSA.cpp
@@ -41,7 +41,7 @@ private:
         {'F', 5}, {'G', 6}, {'H', 7}, {'I', 8}, {'J', 9},
         {'K', 10}, {'L', 11}, {'M', 12}, {'N', 13}, {'O', 14},
         {'P', 15}, {'Q', 16}, {'R', 17}, {'S', 18}, {'T', 19},
-        {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25}, {'-', 26}
+        {'U', 20}, {'V', 21}, {'W', 22}, {'X', 23}, {'Y', 24}, {'Z', 25}, {NULL_CHAR, 26}
         }),
       num_char({
         {0, 'A'}, {1, 'B'}, {2, 'C'}, {3, 'D'}, {4, 'E'},
@@ -50,8 +50,10 @@ private:
         {15, 'P'}, {16, 'Q'}, {17, 'R'}, {18, 'S'}, {19, 'T'},
         {20, 'U'}, {21, 'V'}, {22, 'W'}, {23, 'X'}, {24, 'Y'}, {25, 'Z'}, 
         }),
-      base {27} {
+      base {27},
+      NULL_CHAR {'-'} {
     }
+    const char NULL_CHAR;
     std::map<char, BigInt> char_num;
     std::map<BigInt, char> num_char;
     long unsigned int base;
@@ -265,7 +267,7 @@ std::string RSA::decrypt_ciphertext_block(const std::string& block, Codebook* co
   char codes[BLOCK_SIZE_PLAINTEXT_BYTES];
 
   for (int i = 0; i<BLOCK_SIZE_PLAINTEXT_BYTES; i++) {
-    if (nums[i] == 26) {
+    if (nums[i] == codebook->char_to_num(codebook->NULL_CHAR)) {
       codes[i] = ' ';
       continue;
     }
