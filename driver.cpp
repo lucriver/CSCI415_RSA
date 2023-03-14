23fe908330d9bf4b997d587c93d5223490508c7c
lucas <lriv@DESKTOP-EN3V0A9>
2023-03-14 13:57:45 -0500

    Better driver

--- driver.cpp
+++ driver.cpp
@@ -7,24 +7,25 @@ void basic_test(RSA);
 
 int main() {
   // specify how many digits our RSA primes should be
-  int prime_digits = 100;
+  int prime_digits = 3;
 
   // initialize the RSA crypto-system
   RSA rsa_0(prime_digits);
 
   // // check initialized class values
-  // rsa_0.debug();
+  rsa_0.debug();
 
   // // basic test 
-  // basic_test(rsa_0);
+  basic_test(rsa_0);
 
-  const std::string file_plaintext = "plaintext_0.txt";
-  const std::string file_ciphertext = "ciphertext_0.txt";
-  const std::string file_decrypted_ciphertext = "decrypted_0.txt";
-  std::cout << "encrypting file: " << file_plaintext << std::endl;
-  rsa_0.file_encrypt(file_plaintext,file_ciphertext);
-  rsa_0.file_decrypt(file_ciphertext,file_decrypted_ciphertext);
-  std::cout << "File decrypted. Output file name: " << file_decrypted_ciphertext << std::endl;
+  // const std::string file_plaintext = "plaintext_0.txt";
+  // const std::string file_ciphertext = "ciphertext_0.txt";
+  // const std::string file_decrypted_ciphertext = "decrypted_0.txt";
+  // std::cout << "encrypting file: " << file_plaintext << std::endl;
+  // rsa_0.file_encrypt(file_plaintext,file_ciphertext);
+  // rsa_0.file_decrypt(file_ciphertext,file_decrypted_ciphertext);
+  // std::cout << "File decrypted. Output file name: " << file_decrypted_ciphertext << std::endl;
+
 
   return 0;
 }
@@ -54,4 +55,12 @@ void basic_test(RSA rsa_0) {
     std::cout << message_2[i] << " -> " << ciphertext << " -> " << decrypted << std::endl;
   }
   std::cout << std::endl;
+
+  const std::string message_3[14] = { "HIM", "YNA", "MEI", "SLU", "CAS", "AND", "ILI", "KET", "TOP", "LAY", "VID", "EOG", "AME", "S--" };
+  for (int i=0;i<14;i++) {
+    std::string ciphertext = rsa_0.temp_encrypt(message_3[i]);
+    std::string decrypted = rsa_0.temp_decrypt(ciphertext);
+    std::cout << message_3[i] << " -> " << ciphertext << " -> " << decrypted << std::endl;
+  }
+  std::cout << std::endl; 
 }
